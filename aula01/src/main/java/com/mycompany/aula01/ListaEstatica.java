1963b988b4dbd25b7c032798c21471f241ca3e60
lgonascimento <lgonascimento@MC-5724.furb.br>
2025-07-31 21:42:56 -0300

    comeco listaEstatica

---
+++ aula01/src/main/java/com/mycompany/aula01/ListaEstatica.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula01;
+
+/**
+ *
+ * @author lgonascimento
+ */
+public class ListaEstatica {
+    private int[] info;
+    private int tamanho;
+    
+   public ListaEstatica(){
+       info = new int[10];
+       tamanho = 0;
+   }
+   
+   private void redimensionar(){
+       
+   }
+   
+   public void inserir(int valor) {
+      if()
+   }
+}
