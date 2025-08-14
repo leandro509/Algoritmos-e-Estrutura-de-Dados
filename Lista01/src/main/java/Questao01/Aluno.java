73788bed09c78fe6c99716f401100cadadd036d2
lgonascimento <lgonascimento@MC-5724.furb.br>
2025-08-14 20:19:51 -0300

    correcao lista2

---
+++ Lista01/src/main/java/Questao01/Aluno.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao01;
+
+/**
+ *
+ * @author lgonascimento
+ */
+public class Aluno {
+    private int matricula;
+    private String nome;
+
+    public Aluno(int matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    
+    
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
